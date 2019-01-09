12a508f717c4db7d0cda93d715dad62d86b7a570
lonegunman <lonegunmanb@hotmail.com>
2019-01-09 15:46:32 +0800

    init commit

---
+++ ast/test_util.go
@@ -0,0 +1,16 @@
+package ast
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const testPkgPath = "github.com/lonegunmanb/varys/ast"
+
+func parseCode(t *testing.T, sourceCode string) *typeWalker {
+	typeWalker := NewTypeWalker().(*typeWalker)
+
+	err := typeWalker.Parse(testPkgPath, sourceCode)
+	assert.Nil(t, err)
+	return typeWalker
+}
